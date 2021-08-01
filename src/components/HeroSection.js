e19151173b96754b03e7bd4b0ce5bc5451c63f5c
AbdelRahmanKhalil <abdelrahmankhalil1@gmail.com>
2021-08-01 15:20:07 +0200

    Created the hero section(doesn't take info dynamic

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,6 +3,7 @@ import '../App.css';
 import './HeroSection.css';
 
 function HeroSection() {
+    
     return (
         <div className='hero-container'>
             <video src="/intro.mp4" autoPlay loop muted />
